fa6305c6f2d67fd6299d7809dbd281ca44f0efcf
CyberPaddy <teemu@paddy.fi>
2023-07-25 23:43:12 +0300

    Add IDs to course object

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,19 +5,23 @@ const Total = ({countList}) => <p>Number of exercises {countList.reduce((a, b) =
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
