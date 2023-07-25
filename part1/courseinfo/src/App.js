8a9b17899c450e56bb496de48875311dea0114c1
CyberPaddy <teemu@paddy.fi>
2023-07-25 17:39:06 +0300

    Refactor parts to a list of objects

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,34 +1,31 @@
 const Header = (props) => <h1>{props.course}</h1>
 const Part = (props) => <p>{props.name} {props.count}</p>
-const Content = (props) => <Part name={props.part.name} count={props.part.exercises} />
+const Content = ({parts}) => parts.map((part, index) => <Part key={index} name={part.name} count={part.exercises} />)
 const Total = (props) => <p>Number of exercises {props.countList.reduce((a, b) => a + b, 0)}</p>
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  const exerciseCounts = parts.map(part => part.exercises);
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} />
-      <Content part={part2} />
-      <Content part={part3} />
-      <Total countList={[
-        part1.exercises,
-        part2.exercises,
-        part3.exercises
-      ]}/>
+      <Content parts={parts} />
+      <Total countList={exerciseCounts}/>
     </div>
   )
 }
