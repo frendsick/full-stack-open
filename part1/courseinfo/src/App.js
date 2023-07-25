8c81fbb2629f4ac901e58dbbf03db6b10e27f65e
CyberPaddy <teemu@paddy.fi>
2023-07-25 17:24:58 +0300

    Complete exercise 1.3
    Refactor part-related data to objects

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,24 +1,34 @@
 const Header = (props) => <h1>{props.course}</h1>
 const Part = (props) => <p>{props.name} {props.count}</p>
-const Content = (props) => <Part name={props.part} count={props.count} />
+const Content = (props) => <Part name={props.part.name} count={props.part.exercises} />
 const Total = (props) => <p>Number of exercises {props.countList.reduce((a, b) => a + b, 0)}</p>
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} count={exercises1} />
-      <Content part={part2} count={exercises2} />
-      <Content part={part3} count={exercises3} />
-      <Total countList={[exercises1, exercises2, exercises3]}/>
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+      <Total countList={[
+        part1.exercises,
+        part2.exercises,
+        part3.exercises
+      ]}/>
     </div>
   )
 }
