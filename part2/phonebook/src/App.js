6190b8a40ceccd6ba0569a4ac697cb5ffebe461b
CyberPaddy <teemu@paddy.fi>
2023-07-26 23:47:52 +0300

    Working phonebook without input sanitation

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
+  // Input field states
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  // Input field onChange functions
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+
+  const addPerson = (event) => {
+    event.preventDefault(); // Do not submit the form
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1, // TODO: Create better way of generating ID
+    };
+    setPersons([...persons, newPerson]);
+
+    // Empty the input fields
+    setNewName('');
+    setNewNumber('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map((person) => {
+          return (
+            <Person key={person.id} name={person.name} number={person.number} />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default App
