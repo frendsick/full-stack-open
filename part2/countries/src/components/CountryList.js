030a1e6f9624bf8af3ad134611b46f1edb544ac3
Teemu Pätsi <teemu@paddy.fi>
2023-07-27 23:09:29 +0300

    Add show buttons to each country in the list

--- part2/countries/src/components/CountryList.js
+++ part2/countries/src/components/CountryList.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import Country from "./Country";
 
 const CountryList = ({ countries, filter }) => {
+    const [highlightedCountry, setHighlightedCountry] = useState(null);
     if (countries === null) return;
+
+    const handleCountryClick = (country) => {
+        setHighlightedCountry(country);
+    };
 
     // Only show the countries which common name matches with the filter
     const filteredCountries = countries.filter((country) =>
@@ -10,7 +16,25 @@ const CountryList = ({ countries, filter }) => {
     if (filteredCountries.length > 10) return <p>Too many matches, specify another filter</p>;
     if (filteredCountries.length === 0) return <p>The filter does not match any country</p>;
     if (filteredCountries.length === 1) return <Country country={filteredCountries[0]} />;
-    return filteredCountries.map((country, index) => <p key={index}>{country.name.common}</p>);
+
+    // List countries with a show button for the full information
+    return (
+        <>
+            {filteredCountries.map((country, index) => {
+                return (
+                    <div key={index}>
+                        {country.name.common}
+                        &nbsp;
+                        <button onClick={() => handleCountryClick(country)}>show</button>
+                    </div>
+                );
+            })}
+            {
+                // Render Country only when a country is highlighted
+                highlightedCountry && <Country country={highlightedCountry} />
+            }
+        </>
+    );
 };
 
 export default CountryList;
