4b6e6b36d6d175ba0e0e32e1e97ab78b5c9a1b81
Teemu Pätsi <teemu@paddy.fi>
2023-07-27 17:13:11 +0300

    Implement remove API function for persons

--- part2/phonebook/src/api/person.js
+++ part2/phonebook/src/api/person.js
@@ -3,6 +3,7 @@ const PERSON_API = "http://localhost:3001/persons";
 
 const getAll = () => axios.get(PERSON_API).then((response) => response.data);
 const create = (person) => axios.post(PERSON_API, person).then((response) => response.data);
+const remove = (personId) => axios.delete(`${PERSON_API}/${personId}`);
 
-const exportedFunctions = { getAll, create };
+const exportedFunctions = { getAll, create, remove };
 export default exportedFunctions;
