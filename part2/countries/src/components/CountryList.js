1e451221586dd38928857c793b1c9c1643c2c9b6
Teemu Pätsi <teemu@paddy.fi>
2023-07-27 22:45:57 +0300

    Unique key for each country

--- part2/countries/src/components/CountryList.js
+++ part2/countries/src/components/CountryList.js
@@ -10,7 +10,7 @@ const CountryList = ({ countries, filter }) => {
     if (filteredCountries.length > 10) return <p>Too many matches, specify another filter</p>;
     if (filteredCountries.length === 0) return <p>The filter does not match any country</p>;
     if (filteredCountries.length === 1) return <Country country={filteredCountries[0]} />;
-    return filteredCountries.map((country) => <p>{country.name.common}</p>);
+    return filteredCountries.map((country, index) => <p key={index}>{country.name.common}</p>);
 };
 
 export default CountryList;
