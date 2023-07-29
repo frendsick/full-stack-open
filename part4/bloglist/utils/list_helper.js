32078d4c0bf955fe3a93abe8e24a2addb0a305db
CyberPaddy <teemu@paddy.fi>
2023-07-29 20:11:38 +0300

    Test the like count calculations

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,13 @@
+// Exercise 4.3.
 const dummy = (blogs) => {
     return 1;
 };
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((total, blog) => total + blog.likes, 0);
+};
+
 module.exports = {
     dummy,
+    totalLikes,
 };
