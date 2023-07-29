c6e4eba1ad48dcc5bf49902099f8c52a5909eedb
CyberPaddy <teemu@paddy.fi>
2023-07-29 20:17:31 +0300

    Implement favoriteBlog helper function

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,11 +3,20 @@ const dummy = (blogs) => {
     return 1;
 };
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null;
+
+    return blogs.reduce((maxLikesBlog, blog) => {
+        return blog.likes > maxLikesBlog.likes ? blog : maxLikesBlog;
+    }, blogs[0]);
+};
+
 const totalLikes = (blogs) => {
     return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
 module.exports = {
     dummy,
+    favoriteBlog,
     totalLikes,
 };
