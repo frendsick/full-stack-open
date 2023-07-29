3574b27c09fc4b80f6a8ef65e9782e5cfc8b4caa
CyberPaddy <teemu@paddy.fi>
2023-07-30 02:37:57 +0300

    Test for empty database returning empty list

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -5,13 +5,16 @@ const api = supertest(app);
 const mongo = require("../utils/mongo");
 const config = require("./config");
 
-beforeEach(async () => {
-    await mongo.connectDatabase();
-    await mongo.deleteAllBlogs();
-
+async function sendMockBlogs() {
     const initialNotes = config.mockBlogs;
     await mongo.saveBlog(initialNotes[0]);
     await mongo.saveBlog(initialNotes[1]);
+}
+
+beforeEach(async () => {
+    await mongo.connectDatabase();
+    await mongo.deleteAllBlogs();
+    await sendMockBlogs();
 });
 
 test("api: blogs are returned as json", async () => {
@@ -19,6 +22,16 @@ test("api: blogs are returned as json", async () => {
         .get("/api/blog")
         .expect(200)
         .expect("Content-Type", /application\/json/);
+});
+
+describe("total blogs", () => {
+    test("zero when the database is empty", async () => {
+        await mongo.deleteAllBlogs();
+        const response = await api.get("/api/blog");
+        const blogs = response.body;
+        expect(blogs).toBeInstanceOf(Array);
+        expect(blogs.length).toBe(0);
+    });
 });
 
 afterAll(async () => {
