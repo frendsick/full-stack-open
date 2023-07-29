1f6d46931650f2fac42295ff5165422dd62e74d2
CyberPaddy <teemu@paddy.fi>
2023-07-30 01:34:51 +0300

    Move constants inside the function they are used

--- part4/bloglist/utils/mongo.js
+++ part4/bloglist/utils/mongo.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 const Blog = require("../models/blog");
 require("dotenv").config();
 
-const username = process.env.MONGO_USERNAME;
-const password = process.env.MONGO_PASSWORD;
-const mongoClusterUrl = process.env.MONGO_URL;
-const table = "bloglist";
-const mongoUrl = `mongodb+srv://${username}:${password}@${mongoClusterUrl}/${table}?retryWrites=true&w=majority`;
-
 exports.fetchBlogs = () => Blog.find({});
 exports.fetchBlogById = (id) => Blog.findById(id);
 exports.saveBlog = (blog) => new Blog(blog).save();
 exports.deleteBlogById = (id) => Blog.findByIdAndDelete(id);
 exports.connectDatabase = () => {
+    const username = process.env.MONGO_USERNAME;
+    const password = process.env.MONGO_PASSWORD;
+    const mongoClusterUrl = process.env.MONGO_URL;
+    const table = "bloglist";
+    const mongoUrl = `mongodb+srv://${username}:${password}@${mongoClusterUrl}/${table}?retryWrites=true&w=majority`;
+
     return mongoose
         .connect(mongoUrl)
         .then(() => console.log("Connected to MongoDB"))
