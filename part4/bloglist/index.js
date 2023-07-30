e261abe862d43894d5ece90c6a9156d5d78e9a3a
CyberPaddy <teemu@paddy.fi>
2023-07-30 16:31:23 +0300

    Correct databaseName parameter name

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,10 +1,10 @@
 const app = require("./app");
 const mongo = require("./utils/mongo");
 
-const blogsTable = process.env.NODE_ENV === "test" ? "test-bloglist" : "bloglist";
+const DATABASE_NAME = process.env.NODE_ENV === "test" ? "test-bloglist" : "bloglist";
 
 async function main() {
-    await mongo.connectDatabase(blogsTable);
+    await mongo.connectDatabase(DATABASE_NAME);
     const PORT = process.env.PORT || 3003;
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
--- part4/bloglist/utils/mongo.js
+++ part4/bloglist/utils/mongo.js
@@ -14,11 +14,12 @@ exports.updateBlog = async (id, updatedFields) => {
     const updatedBlog = await Blog.findById(id);
     return updatedBlog;
 };
-exports.connectDatabase = (table) => {
+
+exports.connectDatabase = (databaseName) => {
     const username = process.env.MONGO_USERNAME;
     const password = process.env.MONGO_PASSWORD;
     const mongoClusterUrl = process.env.MONGO_URL;
-    const mongoUrl = `mongodb+srv://${username}:${password}@${mongoClusterUrl}/${table}?retryWrites=true&w=majority`;
+    const mongoUrl = `mongodb+srv://${username}:${password}@${mongoClusterUrl}/${databaseName}?retryWrites=true&w=majority`;
 
     return mongoose
         .connect(mongoUrl)
