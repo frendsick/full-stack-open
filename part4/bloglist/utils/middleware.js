a97325fcdf46921a44ccb60db943fb8380b24f76
CyberPaddy <teemu@paddy.fi>
2023-07-30 13:35:09 +0300

    Handle CastError - Unknown blog ID

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,6 +1,8 @@
 const errorHandler = (err, _, res, next) => {
     console.error("Error:", err.message);
     switch (err.name) {
+        case "CastError":
+            return res.status(404).send({ error: "Unknown blog ID" });
         case "ValidationError":
             return res.status(400).send({ error: err.message });
     }
