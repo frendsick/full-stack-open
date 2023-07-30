dea0d16c88282c5a98c14a3b3ab99dd770dd9809
CyberPaddy <teemu@paddy.fi>
2023-07-30 14:42:52 +0300

    Ensure that updateBlog returns the updated Blog

--- part4/bloglist/utils/mongo.js
+++ part4/bloglist/utils/mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Blog = require("../models/blog");
+const enableUpdateValidator = { runValidators: true };
 require("dotenv").config();
 
 exports.fetchAllBlogs = () => Blog.find({});
@@ -8,6 +9,11 @@ exports.saveBlog = (blog) => new Blog(blog).save();
 exports.saveListOfBlogs = (blogs) => Blog.insertMany(blogs);
 exports.deleteBlogById = (id) => Blog.findByIdAndDelete(id);
 exports.deleteAllBlogs = () => Blog.deleteMany({});
+exports.updateBlog = async (id, updatedFields) => {
+    await Blog.findByIdAndUpdate(id, updatedFields, enableUpdateValidator);
+    const updatedBlog = await Blog.findById(id);
+    return updatedBlog;
+};
 exports.connectDatabase = () => {
     const username = process.env.MONGO_USERNAME;
     const password = process.env.MONGO_PASSWORD;
