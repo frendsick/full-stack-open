0eab6305a418e70608345ecff2074aae3f067f3a
CyberPaddy <teemu@paddy.fi>
2023-07-30 18:49:36 +0300

    Show the User information alongside Blogs

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,28 +1,32 @@
 const blogRouter = require("express").Router();
 const mongo = require("../utils/mongo");
+const userConfig = { name: 1, username: 1 }; // Fields to show for User
 
 blogRouter.get("/", async (_, response) => {
-    const blogs = await mongo.fetchAllBlogs();
+    const blogs = await mongo.fetchAllBlogs().populate("user", userConfig);
     response.json(blogs);
 });
 
 blogRouter.post("/", async (request, response) => {
     const newBlog = request.body;
     const addedBlog = await mongo.saveBlog(newBlog);
-    response.status(201).json(addedBlog);
+    const populatedBlog = await addedBlog.populate("user", userConfig);
+    response.status(201).json(populatedBlog);
 });
 
 blogRouter.get("/:id", async (request, response) => {
     const id = request.params.id;
     const blog = await mongo.fetchBlogById(id);
-    response.json(blog);
+    const populatedBlog = await blog.populate("user", userConfig);
+    response.json(populatedBlog);
 });
 
 blogRouter.put("/:id", async (request, response) => {
     const id = request.params.id;
     const updatedFields = request.body;
     const updatedBlog = await mongo.updateBlog(id, updatedFields);
-    response.json(updatedBlog);
+    const populatedBlog = await updatedBlog.populate("user", userConfig);
+    response.json(populatedBlog);
 });
 
 blogRouter.delete("/:id", async (request, response) => {
