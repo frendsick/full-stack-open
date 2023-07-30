de68c7da5a0d347dd6afba913de395ff287af239
CyberPaddy <teemu@paddy.fi>
2023-07-31 02:35:21 +0300

    Extract bloglist to BlogList component

--- part5/bloglist/frontend/src/App.js
+++ part5/bloglist/frontend/src/App.js
@@ -3,6 +3,7 @@ import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import LoginForm from "./components/LoginForm";
 import Title from "./components/Title";
+import BlogList from "./components/BlogList";
 
 const App = () => {
     const [blogs, setBlogs] = useState([]);
@@ -23,10 +24,8 @@ const App = () => {
 
     return (
         <div>
-            <h2>blogs</h2>
-            {blogs.map((blog) => (
-                <Blog key={blog.id} blog={blog} />
-            ))}
+            <h2>Blogs</h2>
+            <BlogList blogs={blogs} />
         </div>
     );
 };
